0e71a25a4986a2e859fa043dbda64a35370d9652
CJ van den Berg <cj@vdbonline.com>
2024-03-06 22:34:07 +0100

    build: migrate to flow-syntax

--- build.zig
+++ build.zig
@@ -74,6 +74,11 @@ pub fn build(b: *std.Build) void {
 
     const themes_dep = b.dependency("themes", .{});
 
+    const syntax_dep = b.dependency("syntax", .{
+        .target = target,
+        .optimize = dependency_optimize,
+    });
+
     const thespian_dep = b.dependency("thespian", .{
         .target = target,
         .optimize = dependency_optimize,
@@ -100,11 +105,6 @@ pub fn build(b: *std.Build) void {
         .imports = &.{
             .{ .name = "thespian", .module = thespian_mod },
         },
-    });
-
-    const tree_sitter_dep = b.dependency("tree-sitter", .{
-        .target = target,
-        .optimize = dependency_optimize,
     });
 
     const color_mod = b.createModule(.{
@@ -135,74 +135,6 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    const syntax_mod = b.createModule(.{
-        .root_source_file = .{ .path = "src/syntax.zig" },
-        .imports = &.{
-            .{ .name = "Buffer", .module = Buffer_mod },
-            .{ .name = "tracy", .module = tracy_mod },
-            .{ .name = "treez", .module = tree_sitter_dep.module("treez") },
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-agda/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-bash/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-c-sharp/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-c/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-cpp/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-css/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-diff/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-dockerfile/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-git-rebase/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-gitcommit/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-go/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-fish/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-haskell/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-html/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-java/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-javascript/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-jsdoc/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-json/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-lua/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-make/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-markdown/tree-sitter-markdown/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-markdown/tree-sitter-markdown-inline/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-nasm/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-ninja/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-nix/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-ocaml/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-openscad/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-org/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-php/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-python/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-purescript/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-regex/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-ruby/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-rust/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-ssh-config/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-scala/queries/scala/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-scheme/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-toml/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-typescript/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-xml/dtd/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-xml/xml/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-zig/queries/highlights.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-ziggy/tree-sitter-ziggy/queries/highlights.scm"),
-
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-cpp/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-gitcommit/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-html/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-javascript/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-lua/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-markdown/tree-sitter-markdown-inline/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-markdown/tree-sitter-markdown/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-nasm/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-nix/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-openscad/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-php/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-purescript/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-purescript/vim_queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-rust/queries/injections.scm"),
-            ts_queryfile(b, tree_sitter_dep, "tree-sitter-zig/queries/injections.scm"),
-        },
-    });
-
     const diff_mod = b.createModule(.{
         .root_source_file = .{ .path = "src/diff.zig" },
         .imports = &.{
@@ -229,7 +161,7 @@ pub fn build(b: *std.Build) void {
             .{ .name = "config", .module = config_mod },
             .{ .name = "log", .module = log_mod },
             .{ .name = "location_history", .module = location_history_mod },
-            .{ .name = "syntax", .module = syntax_mod },
+            .{ .name = "syntax", .module = syntax_dep.module("syntax") },
             .{ .name = "text_manip", .module = text_manip_mod },
             .{ .name = "Buffer", .module = Buffer_mod },
             .{ .name = "ripgrep", .module = ripgrep_mod },
@@ -287,13 +219,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_run_cmd.step);
-}
-
-fn ts_queryfile(b: *std.Build, dep: *std.Build.Dependency, comptime sub_path: []const u8) std.Build.Module.Import {
-    return .{
-        .name = sub_path,
-        .module = b.createModule(.{
-            .root_source_file = dep.path(sub_path),
-        }),
-    };
 }
