d4cab669971f2527375920878c0af62b7282aa20
aidanaden <aidan@u.nus.edu>
2024-09-27 07:36:38 +0800

    fix: remove unnecessary flake commands

--- flake.nix
+++ flake.nix
@@ -69,24 +69,6 @@
       # nix run .#build
       apps.build = env.app [] "zig build \"$@\"";
 
-      # nix run .#test
-      apps.test = env.app [] "zig build test -- \"$@\"";
-
-      # nix run .#docs
-      apps.docs = env.app [] "zig build docs -- \"$@\"";
-
-      # nix run .#deps
-      apps.deps = env.showExternalDeps;
-
-      # nix run .#zon2json
-      apps.zon2json = env.app [env.zon2json] "zon2json \"$@\"";
-
-      # nix run .#zon2json-lock
-      apps.zon2json-lock = env.app [env.zon2json-lock] "zon2json-lock \"$@\"";
-
-      # nix run .#zon2nix
-      apps.zon2nix = env.app [env.zon2nix] "zon2nix \"$@\"";
-
       # nix develop
       devShells.default = env.mkShell {};
     }));
