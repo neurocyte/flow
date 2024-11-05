78f675649461340c841e762eadb6e8b81b0048d3
CJ van den Berg <cj@vdbonline.com>
2024-11-05 21:00:29 +0100

    fix: render syntax using only the first match at a point

--- src/tui/editor.zig
+++ src/tui/editor.zig
@@ -1079,9 +1079,13 @@ pub const Editor = struct {
             cache: *StyleCache,
             root: Buffer.Root,
             pos_cache: PosToWidthCache,
-            fn cb(ctx: *@This(), range: syntax.Range, scope: []const u8, id: u32, _: usize, _: *const syntax.Node) error{Stop}!void {
+            last_begin: Cursor = .{},
+            fn cb(ctx: *@This(), range: syntax.Range, scope: []const u8, id: u32, idx: usize, _: *const syntax.Node) error{Stop}!void {
                 const sel_ = ctx.pos_cache.range_to_selection(range, ctx.root, ctx.self.metrics) orelse return;
-
+                
+                if (idx > 0) return;
+                if (sel_.begin.eql(ctx.last_begin)) return;
+                ctx.last_begin = sel_.begin;
                 const style_ = style_cache_lookup(ctx.theme, ctx.cache, scope, id);
                 const style = if (style_) |sty| sty.style else return;
                 var sel = sel_;
