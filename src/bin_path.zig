781d43cb3521955ccf72d9282c8acd60295cfdad
Arian <arianxdehghani@gmail.com>
2025-02-20 09:57:04 -0800

    Search windows path using %PATHEXT% instead of hardcoding extensions

--- src/bin_path.zig
+++ src/bin_path.zig
@@ -28,18 +28,23 @@ fn find_binary_in_path_windows(allocator: std.mem.Allocator, binary_name_: []con
         error.EnvironmentVariableNotFound, error.InvalidWtf8 => &.{},
     };
     defer allocator.free(bin_paths);
-    const extensions = [_][]const u8{".exe", ".cmd"};
-    for (extensions) |extension| {
-        var path = std.ArrayList(u8).init(allocator);
-        try path.appendSlice(binary_name_);
-        try path.appendSlice(extension);
-        const binary_name = try path.toOwnedSlice();
-        defer allocator.free(binary_name);
-        var bin_path_iterator = std.mem.splitScalar(u8, bin_paths, std.fs.path.delimiter);
-        while (bin_path_iterator.next()) |bin_path| {
-            if (!std.fs.path.isAbsolute(bin_path)) continue;
-            var dir = std.fs.openDirAbsolute(bin_path, .{}) catch continue;
-            defer dir.close();
+    const bin_extensions = std.process.getEnvVarOwned(allocator, "PATHEXT") catch |err| switch (err) {
+        error.OutOfMemory => return error.OutOfMemory,
+        error.EnvironmentVariableNotFound, error.InvalidWtf8 => &.{},
+    };
+    defer allocator.free(bin_extensions);
+    var bin_path_iterator = std.mem.splitScalar(u8, bin_paths, std.fs.path.delimiter);
+    while (bin_path_iterator.next()) |bin_path| {
+        if (!std.fs.path.isAbsolute(bin_path)) continue;
+        var dir = std.fs.openDirAbsolute(bin_path, .{}) catch continue;
+        defer dir.close();
+        var bin_extensions_iterator = std.mem.splitScalar(u8, bin_extensions, ';');
+        while (bin_extensions_iterator.next()) |bin_extension| {
+            var path = std.ArrayList(u8).init(allocator);
+            try path.appendSlice(binary_name_);
+            try path.appendSlice(bin_extension);
+            const binary_name = try path.toOwnedSlice();
+            defer allocator.free(binary_name);
             _ = dir.statFile(binary_name) catch continue;
             const resolved_binary_path = try std.fs.path.join(allocator, &[_][]const u8{ bin_path, binary_name });
             defer allocator.free(resolved_binary_path);
