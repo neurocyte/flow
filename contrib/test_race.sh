8211be2754dc2051ab59f154c7d6f69f9e9de6de
CJ van den Berg <cj@vdbonline.com>
2025-04-30 14:57:09 +0200

    build: add strace output to test_race.sh script

--- contrib/test_race.sh
+++ contrib/test_race.sh
@@ -9,5 +9,9 @@ fi
 
 for i in {1..60}; do
     echo "running $i ..."
-    flow --exec quit "$@" || exit 1
+    # flow --exec quit "$@" || exit 1
+    strace -f -t \
+        -e trace=open,openat,close,socket,pipe,pipe2,dup,dup2,dup3,fcntl,accept,accept4,epoll_create,epoll_create1,eventfd,timerfd_create,signalfd,execve,fork \
+        -o trace.log \
+        flow --exec quit --trace-level 9 "$@"
 done
