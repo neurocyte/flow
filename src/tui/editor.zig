b1a66698e7a0137743e4d13374aaf3256d561510
Robert Burnett <dr.slate2@gmail.com>
2025-05-11 20:44:09 -0500

    added cut_buffer_begin command and vim binding

--- src/tui/editor.zig
+++ src/tui/editor.zig
@@ -2970,6 +2970,15 @@ pub const Editor = struct {
     }
     pub const cut_buffer_end_meta: Meta = .{ .description = "Cut to the end of the buffer (copies cut text into clipboard)" };
 
+    pub fn cut_buffer_begin(self: *Self, _: Context) Result {
+        const b = try self.buf_for_update();
+        const text, const root = try self.cut_to(move_cursor_buffer_begin, b.root);
+        self.set_clipboard_internal(text);
+        try self.update_buf(root);
+        self.clamp();
+    }
+    pub const cut_buffer_begin_meta: Meta = .{ .description = "Cut to the beginning of the buffer (copies cut text into clipboard)" };
+
     pub fn cut_word_left_vim(self: *Self, _: Context) Result {
         const b = try self.buf_for_update();
         const text, const root = try self.cut_to(move_cursor_word_left_vim, b.root);
