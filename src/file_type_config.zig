de6ca62f6de302614d930091690ff2d09e6a91c2
CJ van den Berg <cj@vdbonline.com>
2025-08-09 18:17:15 +0200

    fix: allocate file_type_name in file_type_config cache

--- src/file_type_config.zig
+++ src/file_type_config.zig
@@ -84,7 +84,7 @@ pub fn get(file_type_name: []const u8) !?@This() {
                 break :file_type if (syntax.FileType.get_by_name_static(file_type_name)) |ft| from_file_type(ft) else null;
             }
         };
-        try cache.put(cache_allocator, file_type_name, file_type);
+        try cache.put(cache_allocator, try cache_allocator.dupe(u8, file_type_name), file_type);
         break :self file_type;
     };
 }
