59921d8e1073aebff7854b014d3bff4f2f4a5598
CJ van den Berg <cj@vdbonline.com>
2025-09-17 10:04:27 +0200

    feat: restore cursor column when cancelling goto mini mode
    This commit refactors the numeric_input mini mode to make the input value
    type generic. This allows the goto mini mode to store the origin column
    along with the row. Also, this will allow more complex numeric_input modes,
    for example a goto mini mode that supports column and row.

--- src/tui/mode/mini/goto.zig
+++ src/tui/mode/mini/goto.zig
@@ -1,3 +1,4 @@
+const fmt = @import("std").fmt;
 const command = @import("command");
 
 const tui = @import("../../tui.zig");
@@ -5,13 +6,43 @@ const tui = @import("../../tui.zig");
 pub const Type = @import("numeric_input.zig").Create(@This());
 pub const create = Type.create;
 
+pub const ValueType = @import("../../editor.zig").Cursor;
+
 pub fn name(_: *Type) []const u8 {
     return "＃goto";
 }
 
-pub fn start(_: *Type) usize {
-    const editor = tui.get_active_editor() orelse return 1;
-    return editor.get_primary().cursor.row + 1;
+pub fn start(_: *Type) ValueType {
+    const editor = tui.get_active_editor() orelse return .{};
+    return editor.get_primary().cursor;
+}
+
+pub fn process_digit(self: *Type, digit: u8) void {
+    switch (digit) {
+        0 => {
+            if (self.input) |*x| x.row = x.row * 10;
+        },
+        1...9 => {
+            if (self.input) |*x| {
+                x.row = x.row * 10 + digit;
+            } else {
+                self.input = .{ .row = digit };
+            }
+        },
+        else => unreachable,
+    }
+}
+
+pub fn delete(self: *Type, input: *ValueType) void {
+    const newval = if (input.row < 10) 0 else input.row / 10;
+    if (newval == 0) self.input = null else input.row = newval;
+}
+
+pub fn format_value(_: *Type, input: ?ValueType, buf: []u8) []const u8 {
+    return if (input) |value|
+        (fmt.bufPrint(buf, "{d}", .{value.row}) catch "")
+    else
+        "";
 }
 
 pub const preview = goto;
@@ -19,5 +50,9 @@ pub const apply = goto;
 pub const cancel = goto;
 
 fn goto(self: *Type, _: command.Context) void {
-    command.executeName("goto_line", command.fmt(.{self.input orelse self.start})) catch {};
+    if (self.input) |input| {
+        command.executeName("goto_line", command.fmt(.{input.row})) catch {};
+    } else {
+        command.executeName("goto_line_and_column", command.fmt(.{ self.start.row, self.start.col })) catch {};
+    }
 }
