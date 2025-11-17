3d0a0571c255936eab01bac634e0dfe26323d6c0
Igor Támara <igor.tamara@gmail.com>
2025-11-17 17:44:01 -0500

    fix: removed unused code

--- src/tui/mode/mini/match.zig
+++ src/tui/mode/mini/match.zig
@@ -21,8 +21,6 @@ pub fn process_egc(self: *Type, egc: []const u8) command.Result {
     var action: []const u8 = "";
     if ((self.ctx.args.match(.{tp.extract(&action)}) catch false)) {
         try command.executeName(action, command.fmt(.{egc}));
-    } else {
-        try command.executeName("match_brackets", .{});
     }
     try command.executeName("exit_mini_mode", .{});
 }
