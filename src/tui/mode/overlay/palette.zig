neurocyte/flow

commit 1600bf6406ae071dda220d21d12263e7e5305cd3

fix: task_palette should use the correct mode hints for width calculations

src/tui/mode/overlay/palette.zig
@@ -27,6 +27,7 @@ pub fn Create(options: type) type {
         allocator: std.mem.Allocator,
         modal: *ModalBackground.State(*Self),
         menu: *Menu.State(*Self),
+        mode: keybind.Mode,
         inputbox: *InputBox.State(*Self),
         logger: log.Logger,
         longest: usize = 0,
@@ -83,13 +84,20 @@ pub fn Create(options: type) type {
                 }))).dynamic_cast(InputBox.State(*Self)) orelse unreachable,
                 .view_rows = get_view_rows(tui.screen()),
                 .entries = .empty,
+                .mode = try keybind.mode("overlay/palette", allocator, .{
+                    .insert_command = "overlay_insert_bytes",
+                }),
             };
             try self.commands.init(self);
+            self.mode.event_handler = EventHandler.to_owned(self);
+            self.mode.name = options.name;
             if (self.menu.scrollbar) |scrollbar| scrollbar.style_factory = scrollbar_style;
             self.longest_hint = if (@hasDecl(options, "load_entries_with_args"))
                 try options.load_entries_with_args(self, ctx)
             else
                 try options.load_entries(self);
+            if (self.entries.items.len > 0)
+                self.initial_selected = self.menu.selected;
             if (@hasDecl(options, "restore_state"))
                 options.restore_state(self) catch {};
             if (@hasDecl(options, "initial_query")) blk: {
@@ -101,12 +109,7 @@ pub fn Create(options: type) type {
             try self.start_query(0);
             try mv.floating_views.add(self.modal.widget());
             try mv.floating_views.add(self.menu.container_widget);
-            var mode = try keybind.mode("overlay/palette", allocator, .{
-                .insert_command = "overlay_insert_bytes",
-            });
-            mode.event_handler = EventHandler.to_owned(self);
-            mode.name = options.name;
-            return mode;
+            return self.mode;
         }
 
         pub fn deinit(self: *Self) void {

src/tui/mode/overlay/task_palette.zig
@@ -34,7 +34,7 @@ pub fn load_entries(palette: *Type) !usize {
         (try palette.entries.addOne(palette.allocator)).* = .{ .label = try palette.allocator.dupe(u8, task) };
         longest = @max(longest, task.len);
     }
-    const hints = if (tui.input_mode()) |m| m.keybind_hints else @panic("no keybind hints");
+    const hints = palette.mode.keybind_hints;
     var longest_hint: usize = 0;
     longest_hint = @max(longest_hint, try add_palette_command(palette, "add_task", hints));
     longest_hint = @max(longest_hint, try add_palette_command(palette, "palette_menu_delete_item", hints));

commit 9597dd7b6da260581930c30d3a36e0b2f3545c30

fix: regression by bad merge in 1600bf6

src/tui/mode/overlay/palette.zig
@@ -111,8 +111,6 @@ pub fn Create(options: type) type {
                 try options.load_entries_with_args(self, ctx)
             else
                 try options.load_entries(self);
-            if (self.entries.items.len > 0)
-                self.initial_selected = self.menu.selected;
             if (@hasDecl(options, "restore_state"))
                 options.restore_state(self) catch {};
             if (@hasDecl(options, "initial_query")) blk: {