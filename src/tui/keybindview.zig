65c9b41784060dd8faa2110edd3bcdad9052e0ce
CJ van den Berg <cj@vdbonline.com>
2025-12-10 15:56:42 +0100

    refactor: split out keybind events from inputview to a new keybindview

--- src/tui/keybindview.zig
+++ src/tui/keybindview.zig
@@ -8,18 +8,17 @@ const tp = @import("thespian");
 const cbor = @import("cbor");
 
 const Plane = @import("renderer").Plane;
-const EventHandler = @import("EventHandler");
 const input = @import("input");
 
 const tui = @import("tui.zig");
 const Widget = @import("Widget.zig");
+const MessageFilter = @import("MessageFilter.zig");
 
-pub const name = "inputview";
+pub const name = "keybindview";
 
 allocator: Allocator,
 parent: Plane,
 plane: Plane,
-last_count: u64 = 0,
 buffer: Buffer,
 
 const Self = @This();
@@ -27,7 +26,7 @@ const Self = @This();
 const Entry = struct {
     time: i64,
     tdiff: i64,
-    json: [:0]u8,
+    msg: []u8,
 };
 const Buffer = ArrayList(Entry);
 
@@ -42,14 +41,16 @@ pub fn create(allocator: Allocator, parent: Plane) !Widget {
         .plane = n,
         .buffer = .empty,
     };
-    try tui.input_listeners().add(EventHandler.bind(self, listen));
+    try tui.message_filters().add(MessageFilter.bind(self, keybind_match));
+    tui.enable_match_events();
     return Widget.to(self);
 }
 
 pub fn deinit(self: *Self, allocator: Allocator) void {
-    tui.input_listeners().remove_ptr(self);
+    tui.disable_match_events();
+    tui.message_filters().remove_ptr(self);
     for (self.buffer.items) |item|
-        self.allocator.free(item.json);
+        self.allocator.free(item.msg);
     self.buffer.deinit(self.allocator);
     self.plane.deinit();
     allocator.destroy(self);
@@ -66,10 +67,8 @@ pub fn render(self: *Self, theme: *const Widget.Theme) bool {
     for (self.buffer.items[begin_at..]) |item| {
         if (first) first = false else _ = self.plane.putstr("\n") catch return false;
         self.output_tdiff(item.tdiff) catch return false;
-        _ = self.plane.putstr(item.json) catch return false;
+        _ = self.plane.putstr(item.msg) catch return false;
     }
-    if (self.last_count > 0)
-        _ = self.plane.print(" ({})", .{self.last_count}) catch {};
     return false;
 }
 
@@ -85,49 +84,29 @@ fn output_tdiff(self: *Self, tdiff: i64) !void {
     }
 }
 
-fn append(self: *Self, json: []const u8) !void {
+fn append(self: *Self, msg: []const u8) !void {
     const ts = time.microTimestamp();
-    const tdiff = if (self.buffer.getLastOrNull()) |last| ret: {
-        if (eql(u8, json, last.json)) {
-            self.last_count += 1;
-            return;
-        }
-        break :ret ts - last.time;
-    } else 0;
-    self.last_count = 0;
+    const tdiff = if (self.buffer.items.len > 0) ts -| self.buffer.items[self.buffer.items.len - 1].time else 0;
     (try self.buffer.addOne(self.allocator)).* = .{
         .time = ts,
         .tdiff = tdiff,
-        .json = try self.allocator.dupeZ(u8, json),
+        .msg = try self.allocator.dupeZ(u8, msg),
     };
 }
 
-fn listen(self: *Self, _: tp.pid_ref, m: tp.message) tp.result {
-    if (try m.match(.{ "M", tp.more })) return;
-    var buf: [4096]u8 = undefined;
-    const json = m.to_json(&buf) catch |e| return tp.exit_error(e, @errorReturnTrace());
+fn keybind_match(self: *Self, _: tp.pid_ref, m: tp.message) MessageFilter.Error!bool {
+    var namespace: []const u8 = undefined;
+    var section: []const u8 = undefined;
+    var cmds: []const u8 = undefined;
+    if (!(m.match(.{ "K", tp.extract(&namespace), tp.extract(&section), tp.extract_cbor(&cmds) }) catch false)) return false;
+
     var result: Writer.Allocating = .init(self.allocator);
     defer result.deinit();
     const writer = &result.writer;
-    writer.writeAll(json) catch |e| return tp.exit_error(e, @errorReturnTrace());
+    cbor.toJsonWriter(m.buf, writer, .{}) catch return true;
 
-    var event: input.Event = 0;
-    var keypress: input.Key = 0;
-    var keypress_shifted: input.Key = 0;
-    var text: []const u8 = "";
-    var modifiers: input.Mods = 0;
-    if (try m.match(.{
-        "I",
-        tp.extract(&event),
-        tp.extract(&keypress),
-        tp.extract(&keypress_shifted),
-        tp.extract(&text),
-        tp.extract(&modifiers),
-    })) {
-        const key_event = input.KeyEvent.from_message(event, keypress, keypress_shifted, text, modifiers);
-        writer.print(" -> {f}", .{key_event}) catch |e| return tp.exit_error(e, @errorReturnTrace());
-    }
-    self.append(result.written()) catch |e| return tp.exit_error(e, @errorReturnTrace());
+    self.append(result.written()) catch return true;
+    return true;
 }
 
 pub fn receive(_: *Self, _: tp.pid_ref, _: tp.message) error{Exit}!bool {
