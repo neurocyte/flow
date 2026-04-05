c41984641fd9bea9fec0300140681f562921fad0
Ingo Lohmar <ingo.lohmar@posteo.net>
2026-04-05 23:00:40 +0200

    args for scroll_{up,down,left,right}; scroll_step_vertical only for mouse

--- src/tui/editor.zig
+++ src/tui/editor.zig
@@ -3002,17 +3002,15 @@ pub const Editor = struct {
         self.update_animation_step(dest);
     }
 
-    fn scroll_up_internal(self: *Self) void {
-        const scroll_step_vertical = tui.config().scroll_step_vertical;
+    fn scroll_up_internal(self: *Self, count: usize) void {
         var dest_row = self.scroll_dest;
-        dest_row = if (dest_row > scroll_step_vertical) dest_row - scroll_step_vertical else 0;
+        dest_row -|= count;
         self.update_scroll_dest_abs(dest_row);
     }
 
-    fn scroll_down_internal(self: *Self) void {
-        const scroll_step_vertical = tui.config().scroll_step_vertical;
+    fn scroll_down_internal(self: *Self, count: usize) void {
         var dest_row = self.scroll_dest;
-        dest_row += scroll_step_vertical;
+        dest_row += count;
         self.update_scroll_dest_abs(dest_row);
     }
 
@@ -3035,7 +3033,7 @@ pub const Editor = struct {
         else if (tui.fast_scroll())
             self.scroll_pageup()
         else
-            self.scroll_up_internal();
+            self.scroll_up_internal(tui.config().scroll_step_vertical);
     }
 
     pub fn mouse_scroll_down(self: *Self) void {
@@ -3044,7 +3042,7 @@ pub const Editor = struct {
         else if (tui.fast_scroll())
             self.scroll_pagedown()
         else
-            self.scroll_down_internal();
+            self.scroll_down_internal(tui.config().scroll_step_vertical);
     }
 
     pub fn scroll_to(self: *Self, row: usize) void {
@@ -4411,15 +4409,19 @@ pub const Editor = struct {
     }
     pub const unindent_meta: Meta = .{ .description = "Unindent current line", .arguments = &.{.integer} };
 
-    pub fn scroll_up(self: *Self, _: Context) Result {
-        self.scroll_up_internal();
+    pub fn scroll_up(self: *Self, ctx: Context) Result {
+        var count: usize = 1;
+        _ = ctx.args.match(.{tp.extract(&count)}) catch false;
+        self.scroll_up_internal(count);
     }
-    pub const scroll_up_meta: Meta = .{ .description = "Scroll up" };
+    pub const scroll_up_meta: Meta = .{ .description = "Scroll up", .arguments = &.{.integer} };
 
-    pub fn scroll_down(self: *Self, _: Context) Result {
-        self.scroll_down_internal();
+    pub fn scroll_down(self: *Self, ctx: Context) Result {
+        var count: usize = 1;
+        _ = ctx.args.match(.{tp.extract(&count)}) catch false;
+        self.scroll_down_internal(count);
     }
-    pub const scroll_down_meta: Meta = .{ .description = "Scroll down" };
+    pub const scroll_down_meta: Meta = .{ .description = "Scroll down", .arguments = &.{.integer} };
 
     pub fn move_scroll_up(self: *Self, ctx: Context) Result {
         const root = try self.buf_root();
@@ -4437,15 +4439,19 @@ pub const Editor = struct {
     }
     pub const move_scroll_down_meta: Meta = .{ .description = "Move and scroll down", .arguments = &.{.integer} };
 
-    pub fn scroll_left(self: *Self, _: Context) Result {
-        self.view.move_left(tui.config().scroll_step_horizontal) catch {};
+    pub fn scroll_left(self: *Self, ctx: Context) Result {
+        var count: usize = 1;
+        _ = ctx.args.match(.{tp.extract(&count)}) catch false;
+        self.view.move_left(count) catch {};
     }
-    pub const scroll_left_meta: Meta = .{ .description = "Scroll left" };
+    pub const scroll_left_meta: Meta = .{ .description = "Scroll left", .arguments = &.{.integer} };
 
-    pub fn scroll_right(self: *Self, _: Context) Result {
-        self.view.move_right(tui.config().scroll_step_horizontal) catch {};
+    pub fn scroll_right(self: *Self, ctx: Context) Result {
+        var count: usize = 1;
+        _ = ctx.args.match(.{tp.extract(&count)}) catch false;
+        self.view.move_right(count) catch {};
     }
-    pub const scroll_right_meta: Meta = .{ .description = "Scroll right" };
+    pub const scroll_right_meta: Meta = .{ .description = "Scroll right", .arguments = &.{.integer} };
 
     pub fn mouse_scroll_left(self: *Self) void {
         const scroll_step_horizontal = tui.config().scroll_step_horizontal;
