7f148ccd1f93bd93d634a613bccfeb810571c12c
CJ van den Berg <cj@vdbonline.com>
2026-06-03 11:47:46 +0200

    fix(gui): clear minimode cursor

--- src/tui/status/filestate.zig
+++ src/tui/status/filestate.zig
@@ -38,6 +38,7 @@ eol_mode: Buffer.EolMode = .lf,
 utf8_sanitized: bool = false,
 indent_mode: config.IndentMode = .spaces,
 auto_save: bool = false,
+have_mini_mode_cursor: bool = false,
 
 const project_icon = "";
 const Self = @This();
@@ -101,16 +102,22 @@ pub fn render(self: *Self, btn: *ButtonType, theme: *const Widget.Theme) bool {
         btn.plane.home();
     }
     if (tui.mini_mode()) |_|
-        render_mini_mode(&btn.plane, theme)
+        self.render_mini_mode(&btn.plane, theme)
     else if (self.detailed)
         self.render_detailed(&btn.plane, theme, auto_save)
     else
         self.render_normal(&btn.plane, theme, auto_save);
+
+    if (tui.mini_mode() == null and self.have_mini_mode_cursor) {
+        self.have_mini_mode_cursor = false;
+        btn.plane.cursor_disable();
+    }
+
     self.render_terminal_title();
     return false;
 }
 
-fn render_mini_mode(plane: *Plane, theme: *const Widget.Theme) void {
+fn render_mini_mode(self: *Self, plane: *Plane, theme: *const Widget.Theme) void {
     plane.off_styles(styles.italic);
     const mini_mode = tui.mini_mode() orelse return;
     _ = plane.putstr_unicode(" ") catch {};
@@ -119,6 +126,7 @@ fn render_mini_mode(plane: *Plane, theme: *const Widget.Theme) void {
         const pos: c_int = @intCast(cursor);
         if (tui.has_native_cursor()) {
             plane.cursor_enable(0, pos + 1, tui.get_cursor_shape());
+            self.have_mini_mode_cursor = true;
         } else {
             plane.cursor_move_yx(0, pos + 1);
             var cell = plane.cell_init();
