1f5fe15584b12e95fa2f29e83a24eeaf242ff2d2
Xefreh <william.strainchamps@gmail.com>
2026-07-12 23:46:42 +0200

    feat: add default language server and formatter for java
    Set jdtls as the default language server and google-java-format as the
    default formatter for the java file type.

--- src/file_type_lsp.zig
+++ src/file_type_lsp.zig
@@ -105,7 +105,10 @@ pub const superhtml = .{
 
 pub const hurl = .{};
 
-pub const java = .{};
+pub const java = .{
+    .language_server = .{"jdtls"},
+    .formatter = .{ "google-java-format", "-" },
+};
 
 pub const javascript = .{
     .language_server = .{ "typescript-language-server", "--stdio" },
