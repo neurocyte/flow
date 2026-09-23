neurocyte/flow

commit a00e8a030a2f7135a81ab8bc5a898110288338b6

refactor: improve robustness of config directory handling

src/main.zig
@@ -949,7 +949,11 @@ fn write_config_file_atomic(file_name: []const u8, content: []const u8) !void {
 }
 
 pub fn list_themes(allocator: std.mem.Allocator) ![]const []const u8 {
-    var dir = try std.Io.Dir.openDirAbsolute(global_init.io, try get_theme_directory(), .{ .iterate = true });
+    const theme_directory = try get_theme_directory();
+    var dir = std.Io.Dir.openDirAbsolute(global_init.io, theme_directory, .{ .iterate = true }) catch |e| switch (e) {
+        error.FileNotFound => return &.{},
+        else => return e,
+    };
     defer dir.close(global_init.io);
     var result: std.ArrayList([]const u8) = .empty;
     var iter = dir.iterate();
@@ -969,14 +973,16 @@ pub fn get_config_dir() ConfigDirError![]const u8 {
 pub const ConfigDirError = error{
     NoSpaceLeft,
     MakeConfigDirFailed,
-    MakeHomeConfigDirFailed,
-    MakeAppConfigDirFailed,
     AppConfigDirUnavailable,
 };
 
 fn make_dir_error(path: []const u8, err: anytype) @TypeOf(err) {
     std.log.err("failed to create directory: '{s}'", .{path});
     return err;
+}
+
+fn create_dir_path(io: std.Io, path: []const u8) std.Io.Dir.CreateDirPathError!void {
+    return std.Io.Dir.cwd().createDirPath(io, path);
 }
 
 fn get_app_config_dir(appname: []const u8) ConfigDirError![]const u8 {
@@ -993,34 +999,22 @@ fn get_app_config_dir(appname: []const u8) ConfigDirError![]const u8 {
     } else if (environ.get("XDG_CONFIG_HOME")) |xdg| ret: {
         break :ret try std.fmt.bufPrint(&local.config_dir_buffer, "{s}{c}{s}", .{ xdg, sep, appname });
     } else if (environ.get("HOME")) |home| ret: {
-        const dir = try std.fmt.bufPrint(&local.config_dir_buffer, "{s}{c}.config", .{ home, sep });
-        std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-            error.PathAlreadyExists => {},
-            else => return make_dir_error(dir, error.MakeHomeConfigDirFailed),
-        };
         break :ret try std.fmt.bufPrint(&local.config_dir_buffer, "{s}{c}.config{c}{s}", .{ home, sep, sep, appname });
     } else if (builtin.os.tag == .windows) ret: {
         if (environ.get("APPDATA")) |appdata| {
-            const dir = try std.fmt.bufPrint(&local.config_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
-            std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-                error.PathAlreadyExists => {},
-                else => return make_dir_error(dir, error.MakeAppConfigDirFailed),
-            };
-            break :ret dir;
+            break :ret try std.fmt.bufPrint(&local.config_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
         } else return error.AppConfigDirUnavailable;
     } else return error.AppConfigDirUnavailable;
 
     local.config_dir = config_dir;
-    std.Io.Dir.createDirAbsolute(io, config_dir, .default_dir) catch |e| switch (e) {
-        error.PathAlreadyExists => {},
-        else => return make_dir_error(config_dir, error.MakeConfigDirFailed),
-    };
+    create_dir_path(io, config_dir) catch
+        return make_dir_error(config_dir, error.MakeConfigDirFailed);
 
     var keybind_dir_buffer: [std.posix.PATH_MAX]u8 = undefined;
-    std.Io.Dir.createDirAbsolute(io, try std.fmt.bufPrint(&keybind_dir_buffer, "{s}{c}{s}", .{ config_dir, sep, keybind_dir }), .default_dir) catch {};
+    create_dir_path(io, try std.fmt.bufPrint(&keybind_dir_buffer, "{s}{c}{s}", .{ config_dir, sep, keybind_dir })) catch {};
 
     var theme_dir_buffer: [std.posix.PATH_MAX]u8 = undefined;
-    std.Io.Dir.createDirAbsolute(io, try std.fmt.bufPrint(&theme_dir_buffer, "{s}{c}{s}", .{ config_dir, sep, theme_dir }), .default_dir) catch {};
+    create_dir_path(io, try std.fmt.bufPrint(&theme_dir_buffer, "{s}{c}{s}", .{ config_dir, sep, theme_dir })) catch {};
 
     return config_dir;
 }
@@ -1041,28 +1035,15 @@ fn get_app_cache_dir(appname: []const u8) ![]const u8 {
     else if (environ.get("XDG_CACHE_HOME")) |xdg| ret: {
         break :ret try std.fmt.bufPrint(&local.cache_dir_buffer, "{s}{c}{s}", .{ xdg, sep, appname });
     } else if (environ.get("HOME")) |home| ret: {
-        const dir = try std.fmt.bufPrint(&local.cache_dir_buffer, "{s}{c}.cache", .{ home, sep });
-        std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-            error.PathAlreadyExists => {},
-            else => return make_dir_error(dir, e),
-        };
         break :ret try std.fmt.bufPrint(&local.cache_dir_buffer, "{s}{c}.cache{c}{s}", .{ home, sep, sep, appname });
     } else if (builtin.os.tag == .windows) ret: {
         if (environ.get("APPDATA")) |appdata| {
-            const dir = try std.fmt.bufPrint(&local.cache_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
-            std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-                error.PathAlreadyExists => {},
-                else => return make_dir_error(dir, e),
-            };
-            break :ret dir;
+            break :ret try std.fmt.bufPrint(&local.cache_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
         } else return error.AppCacheDirUnavailable;
     } else return error.AppCacheDirUnavailable;
 
     local.cache_dir = cache_dir;
-    std.Io.Dir.createDirAbsolute(io, cache_dir, .default_dir) catch |e| switch (e) {
-        error.PathAlreadyExists => {},
-        else => return make_dir_error(cache_dir, e),
-    };
+    create_dir_path(io, cache_dir) catch |e| return make_dir_error(cache_dir, e);
     return cache_dir;
 }
 
@@ -1095,33 +1076,15 @@ fn get_app_state_dir(appname: []const u8) ![]const u8 {
     else if (environ.get("XDG_STATE_HOME")) |xdg| ret: {
         break :ret try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}{s}", .{ xdg, sep, appname });
     } else if (environ.get("HOME")) |home| ret: {
-        var dir = try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}.local", .{ home, sep });
-        std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-            error.PathAlreadyExists => {},
-            else => return make_dir_error(dir, e),
-        };
-        dir = try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}.local{c}state", .{ home, sep, sep });
-        std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-            error.PathAlreadyExists => {},
-            else => return make_dir_error(dir, e),
-        };
         break :ret try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}.local{c}state{c}{s}", .{ home, sep, sep, sep, appname });
     } else if (builtin.os.tag == .windows) ret: {
         if (environ.get("APPDATA")) |appdata| {
-            const dir = try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
-            std.Io.Dir.createDirAbsolute(io, dir, .default_dir) catch |e| switch (e) {
-                error.PathAlreadyExists => {},
-                else => return make_dir_error(dir, e),
-            };
-            break :ret dir;
+            break :ret try std.fmt.bufPrint(&local.state_dir_buffer, "{s}{c}{s}", .{ appdata, sep, appname });
         } else return error.AppCacheDirUnavailable;
     } else return error.AppCacheDirUnavailable;
 
     local.state_dir = state_dir;
-    std.Io.Dir.createDirAbsolute(io, state_dir, .default_dir) catch |e| switch (e) {
-        error.PathAlreadyExists => {},
-        else => return make_dir_error(state_dir, e),
-    };
+    create_dir_path(io, state_dir) catch |e| return make_dir_error(state_dir, e);
     return state_dir;
 }
 

src/tui/Widget.zig
@@ -98,7 +98,10 @@ const static_themes = @import("themes").themes;
 fn get_themes(allocator: std.mem.Allocator) *std.StringHashMap(*ThemeInfo) {
     if (themes_) |*themes__| return themes__;
 
-    const theme_files = root.list_themes(allocator) catch @panic("OOM get_themes");
+    const theme_files = root.list_themes(allocator) catch |e| ret: {
+        std.log.err("failed to list themes: {t}", .{e});
+        break :ret &.{};
+    };
     var themes: std.StringHashMap(*ThemeInfo) = .init(allocator);
     defer allocator.free(theme_files);
     for (theme_files) |file| {

src/tui/tui.zig
@@ -272,7 +272,7 @@ fn init(allocator: Allocator) InitError!*Self {
     }
     self.mainview_ = try MainView.create(allocator);
     resize();
-    try save_config();
+    save_config() catch |e| self.logger.err("save_config", e);
     try self.init_input_namespace();
     if (tp.env.get().is("restore-session")) {
         command.executeName("restore_session", .empty()) catch |e| self.logger.err("restore_session", e);
@@ -288,7 +288,7 @@ fn init_input_namespace(self: *Self) InitError!void {
         self.logger.print_err("keybind", "unknown mode {s}", .{namespace_name});
         try keybind.set_namespace("flow");
         self.config_.input_mode = "flow";
-        try save_config();
+        save_config() catch |e| self.logger.err("save_config", e);
     };
 }
 

commit 8983aa215ea432bd9ed9c1d63c77d7174ed2cede

fix: don't fail on symlinked config directories
This regressed in commit a00e8a03

src/main.zig
@@ -1003,7 +1003,11 @@ fn make_dir_error(path: []const u8, err: anytype) @TypeOf(err) {
 }
 
 fn create_dir_path(io: std.Io, path: []const u8) std.Io.Dir.CreateDirPathError!void {
-    return std.Io.Dir.cwd().createDirPath(io, path);
+    std.Io.Dir.cwd().createDirPath(io, path) catch |e| switch (e) {
+        // createDirPath does not follow symlinks on the last component
+        error.NotDir => if (!is_directory(path)) return e,
+        else => return e,
+    };
 }
 
 fn get_app_config_dir(appname: []const u8) ConfigDirError![]const u8 {