70c262e01b3b7fac44ed58fe27d07599677407c1
CJ van den Berg <cj@vdbonline.com>
2026-09-23 14:59:26 +0200

    fix(tasks): prevent instert of empty tasks

--- src/tui/mode/overlay/palette.zig
+++ src/tui/mode/overlay/palette.zig
@@ -721,7 +721,7 @@ pub fn Create(options: type) type {
             pub fn palette_menu_insert(self: *Self, _: Ctx) Result {
                 const activate = self.activate;
                 self.activate = .normal;
-                if (has_insert)
+                if (has_insert and self.inputbox.text.items.len > 0)
                     return options.insert(activate, self.inputbox.text.items);
             }
             pub const palette_menu_insert_meta: Meta = .{ .icon = "" };
